542c532f9c2e400dd4501bac298c9415b675cfae
Sovego <egor.sofronov03@gmail.com>
2023-11-10 16:56:48 +0700

    lab 6

---
+++ lab6/ex4/lidar_stopping/lidar_stopping/main.py
@@ -0,0 +1,51 @@
+import sys
+import math
+import time
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist, Point, Quaternion
+from tf_transformations import euler_from_quaternion
+
+
+class Turtle(Node):
+    def __init__(self):
+        super().__init__("lidar_stopping_Node")
+        self.publisher_ = self.create_publisher(Twist, '/robot/cmd_vel', 10)
+        self.pose_sub = self.create_subscription(LaserScan, '/robot/scan', self.pose_callback, 1)
+        self.timer = self.create_timer(0.2, self.go_forward)
+        self.msg = LaserScan()
+
+    def pose_callback(self, data):
+        self.msg = data
+
+    def go_forward(self):
+        message = Twist()
+        check = 1
+   
+        for i in range(len(self.msg.ranges)):
+            if(self.msg.ranges[i] < self.msg.range_min and self.msg.ranges[i] > self.msg.range_max):
+                continue
+
+            if(self.msg.ranges[i] < 1.0):
+                check = 0
+                break
+
+        if(check):
+            message.linear.x = 0.1
+        else:
+            message.linear.x = 0.0
+            
+        self.publisher_.publish(message)             
+
+def main(args=None):
+    rclpy.init(args=args)
+    minimal_publisher = Turtle()
+    rclpy.spin(minimal_publisher)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ lab6/ex5/depth_stopping/depth_stopping/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env pythonм
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+from cv_bridge import CvBridge,CvBridgeError
+import cv2
+import numpy as np    
+class Turtle(Node):
+    def __init__(self):
+        super().__init__('depth_stopping_Node')
+        self.publisher_ = self.create_publisher(Twist, '/robot/cmd_vel', 10)
+        self.pose_sub = self.create_subscription(Image, '/depth/image', self.pose_callback, 1)
+        self.timer = self.create_timer(0.2, self.go_forward)
+        self.msg = None
+
+    def pose_callback(self, data):
+        bridge = CvBridge()
+        img = bridge.imgmsg_to_cv2(data,desired_encoding="passthrough")
+        self.msg = img
+
+    def go_forward(self):
+        message = Twist()
+        image = self.msg
+        height, width = image.shape
+        if width!=0:
+            center_x, center_y = width // 2, height // 2
+            num_points = 10
+            points = []
+            for i in range(num_points):
+                angle = 2 * np.pi * i / num_points
+                x = int(center_x + 0.8 * center_x * np.cos(angle))
+                y = int(center_y + 0.8 * center_y * np.sin(angle))
+                points.append((x, y))
+            average_point = (int(np.mean([point[0] for point in points])), int(np.mean([point[1] for point in points])))
+            pixel_values = []
+            for point in points:
+                pixel_values.append(image[point[1], point[0]])
+            average_pixel_value = np.mean(pixel_values, axis=0)
+            #center_color = image[int(self.msg.width*self.msg.height/2+self.msg.width/2)]
+            self.get_logger().info('dist {0} '.format(average_pixel_value))
+       
+            if(average_pixel_value<=1.0):
+                message.linear.x = 0.0
+            else:
+                message.linear.x = 0.1
+            message.angular.z = 0.0
+            self.publisher_.publish(message)    
+
+def main(args=None):
+    rclpy.init(args=args)
+    minimal_publisher = Turtle()
+    rclpy.spin(minimal_publisher)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
